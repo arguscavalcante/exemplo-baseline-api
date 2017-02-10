d4820222a445d21ac5ef73e5fd37ead112317103
EdsonNascimentoJr <egustavo@br.ibm.com>
2017-02-10 09:45:23 -0200

    acerto do inventorio

--- server/sample-data/generate-inventory.js
+++ server/sample-data/generate-inventory.js
@@ -11,38 +11,12 @@ var path = require('path');
 var inventory = [];
 var request = require('request');
 
-request('http://localhost:3000/api/cars', {json: true},
+request('http://exemplo-baseline-api.mybluemix.net/api/cars', {json: true},
   function(err, res, cars) {
     if (err) {
       console.error('Cannot get Cars', err);
       return;
     }
-
-    request('http://localhost:3000/api/locations', {json: true},
-      function(err, res, locations) {
-        if (err) {
-          console.error('Cannot get Locations', err);
-          return;
-        }
-
-        locations.forEach(function(loc) {
-          cars.forEach(function(car) {
-            var availableAtLocation = rand(0, 100);
-
-            inventory.push({
-              productId: car.id,
-              locationId: loc.id,
-              available: rand(0, availableAtLocation),
-              total: availableAtLocation
-            });
-
-          });
-        });
-
-        fs.writeFileSync(
-          path.resolve(__dirname, 'inventory.json'),
-          JSON.stringify(inventory, null, 2));
-      });
   });
 
 function rand(min, max) {
